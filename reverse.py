87fdef16907b6400ad7db759d1c2d2f769245676
Ana Prado <153234400+anapprado@users.noreply.github.com>
2023-12-14 14:37:45 -0600

    Problem1_done

--- reverse.py
+++ reverse.py
@@ -1,7 +1,7 @@
 # MODIFY ME TO IMPLEMENT YOUR SOLUTION
 # TO PROBLEM 1: REVERSE QUEUE
 #
-# NAME:         FIXME
+# NAME:         Ana
 # ASSIGNMENT:   Project 2: Stacks & Queues
 
 from Queue import Queue
@@ -11,8 +11,15 @@ from Stack import Stack
 # Return a new queue in reverse order
 # Hint: can use a stack to help
 def reverse(q_orig):
-    q_new = Queue([])
-    # FIXME
+    q_new = Queue()
+    stack = Stack()
+ 
+    while not q_orig.is_empty(): # why stack push is first?
+        stack.push(q_orig.deq())
+
+    while not stack.is_empty():
+        q_new.enq(stack.pop())
+
     return q_new
 
 def main():
@@ -20,7 +27,7 @@ def main():
     q.print()
     print("reverse: ", end="")
     reverse(q).print()
-
+    
 # Don't run main on import
 if __name__ == "__main__":
     main()
