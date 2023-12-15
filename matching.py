082c83b1be4995a330b44db131b7dc042215a72d
Ana Prado <153234400+anapprado@users.noreply.github.com>
2023-12-14 21:13:54 -0600

    Problem2_done

--- matching.py
+++ matching.py
@@ -1,7 +1,7 @@
 # MODIFY ME TO IMPLEMENT YOUR SOLUTION
 # TO PROBLEM 2: BRACE MATCHER
 #
-# NAME:         FIXME
+# NAME:         Ana
 # ASSIGNMENT:   Project 2: Stacks & Queues
 
 from Stack import Stack
@@ -10,13 +10,35 @@ from Stack import Stack
 # & False otherwise
 def matcher(str):
     s = Stack([])
-    # FIXME
-    return True
+    for char in str:
+        if char in "([{":
+            s.push(char)
+        elif char in ")]}":
+            if  is_matching(s.pop(), char):
+                return True
+            
+    return s.is_empty()
+    
+def is_matching(open,close):
+    return(open == "(" and close == ")") or \
+          (open == "{" and close == "}") or \
+          (open == "[" and close == "]") 
+
+    
 
 def main():
     print("matcher: ", matcher("[()]"))
+    print("matcher: ",matcher("[("))
+    print("matcher: ",matcher("hello"))
+    #print("matcher: ",matcher(" ")) # if it is empty why is true?
 
 # Don't run main on import
 if __name__ == "__main__":
     main()
 
+# This code uses the or operator to combine three conditions. 
+# It checks if either of the following conditions is true
+# The backslash \ at the end of each line is a line 
+# continuation character, indicating that the line 
+# continues to the next line. It allows you to break a long 
+# line of code into multiple lines for better readability.
